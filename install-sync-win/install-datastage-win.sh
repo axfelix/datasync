6781683523ab27263ffe76f3bef85a3f57f78250
axfelix <axfelix@gmail.com>
2013-06-13 11:46:57 -0700

    fixed more embarrassing path bugs

--- install-sync-win/install-datastage-win.sh
+++ install-sync-win/install-datastage-win.sh
@@ -4,4 +4,4 @@ echo 'In the event that you and a colleague are working on the same file simulta
 mkshortcut -n ~/datadisk/backup.lnk ~/.unison/backup
 mkshortcut -n ~/Desktop/datadisk ~/datadisk
 
-echo -e 'if ping -n 1 datadisk.lib.sfu.ca | grep "TTL" ; then\nif ps -W | grep "~[/].datastage"; then\nexit\nelse\nunison.exe ~/Documents/datadisk ssh://'$user'@datadisk.lib.sfu.ca/srv/datastage/private/'$user' -batch -backups -copythreshold 5000 -prefer root -ignore="Name *.tmp" -ignore="Name *~"\nfi\nfi' > ~/.datastage.sh
+echo -e 'if ping -n 1 datadisk.lib.sfu.ca | grep "TTL" ; then\nif ps -W | grep "~[/].datastage"; then\nexit\nelse\nunison.exe ~/Documents/datadisk ssh://'$user'@datadisk.lib.sfu.ca:/home/'$user' -batch -backups -copythreshold 5000 -prefer root -ignore="Name *.tmp" -ignore="Name *~"\nfi\nfi' > ~/.datastage.sh
